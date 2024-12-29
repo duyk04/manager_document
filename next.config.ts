add1c76b9b8efa2a936428bae81608cbf66f92ed
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-12-29 20:37:12 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,23 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// // next.config.js
+// const path = require('path');
+
+// module.exports = {
+//     async rewrites() {
+//         return [
+//             {
+//                 source: '/uploads/:path*',
+//                 destination: '/uploads/:path*',
+//             },
+//         ];
+//     },
+//     // Cấu hình để Next.js nhận diện các file static
+//     webpack(config) {
+//         config.resolve.alias['@uploads'] = path.join(__dirname, 'uploads');
+//         return config;
+//     },
+// };
+
