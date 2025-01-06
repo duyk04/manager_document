cedfa5cdca9c6abb0010e3f3883071c9e98a9e3f
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2025-01-06 17:21:03 +0700

    Update layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,15 +1,13 @@
 import { NavigationNavbar } from "@/components/navigation/navigaton-navbar";
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
-    return (  
+    return (
         <div className="h-full">
-            <div className="flex justify-center w-full py-3 bg-black text-white">
-                <div>
-                   <NavigationNavbar/>
-                </div>
+            <div className="flex justify-center w-full text-white fixed top-0 z-50 bg-white dark:bg-black dark:text-white">
+                <NavigationNavbar />
             </div>
 
-            <div className="w-full">
+            <div className="w-full mt-[4rem]">
                 {children}
             </div>
         </div>
