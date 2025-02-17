6cc8f8ce458d7db7361fd7e0ceb833ef5dfe820c
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2025-02-17 10:47:05 +0700

    Update page.tsx

--- app/(main)/document/view/page.tsx
+++ app/(main)/document/view/page.tsx
@@ -1,9 +1,13 @@
-"use client"
-
 import { ViewDocumentModal } from "@/components/documents/document-list";
+import { currentProfile } from "@/lib/current-profile";
+import { redirect } from "next/navigation";
 
-
-const DocumentPage = () => {
+const DocumentPage = async () => {
+    const profile = await currentProfile();
+    if (!profile) {
+        redirect("/home");
+    }
+    // console.log(profile);
     return (
         <div className="flex flex-col items-center">
             <p className="text-start w-full font-semibold text-2xl pt-5">Danh sách văn bản</p>
