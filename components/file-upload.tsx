bf7365120e7e28cfb76598a8fab8b4501a88e468
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2025-02-24 01:21:43 +0700

    fix ui and few other feature

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,15 +2,20 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FileIcon, X } from "lucide-react";
+import { X } from "lucide-react";
+import { IconExcel, IconPdf, IconWord } from "./ui/file-icon";
 
 interface FileUploadProps {
 	value?: string;
 	onChange?: (filePath: string) => void;
+	typeFile?: '.pdf' | '.doc' | '.docx' | '.xls' | '.xlsx' | '.doc, .docx, .xls, .xlsx';
+	required?: boolean;
 }
 
 export const FileUpload = ({
 	value,
+	typeFile,
+	required,
 	onChange
 }: FileUploadProps) => {
 	const [file, setFile] = useState<File | null>(null);
@@ -53,21 +58,34 @@ export const FileUpload = ({
 	};
 
 	if (response?.status === "success") {
+		const typeFile = response.filePath.split(".").pop();
 		return (
-			<div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
-				<FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+			<div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10 border border-indigo-200 dark:border-indigo-400">
+
+				{(typeFile === "pdf") && (
+					<IconPdf/>
+				)}
+
+				{(typeFile === "doc" || typeFile === "docx") && (
+					<IconWord/>
+				)}
+
+				{(typeFile === "xls" || typeFile === "xlsx") && (
+					<IconExcel/>
+				)}
+
 				<a
 					href={response.filePath}
 					target="_blank"
 					rel="noreferrer noopener"
 					className="ml-2 w-[380px] text-indigo-500 text-sm dark:text-indigo-400 hover:underline overflow-hidden whitespace-nowrap"
 				>
-					{response.filePath}
+					{response.filePath.split("/").pop()}
 				</a>
 				<button
-					onClick={() => {setResponse(null)}}
+					onClick={() => { setResponse(null); onChange && onChange("") }}
 					className="bg-rose-500 text-white p-1
-                    rounded-full absolute top-0 right-0 shadow-sm"
+                    rounded-full absolute top-[-10px] right-[-10px] shadow-sm"
 					type="button"
 				>
 					<X className="h-4 w-4" />
@@ -77,21 +95,32 @@ export const FileUpload = ({
 	}
 
 	if (value) {
+		const typeFile = value.split(".").pop();
 		return (
-			<div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
-				<FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+			<div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10 border border-indigo-200 dark:border-indigo-400">
+				{(typeFile === "pdf") && (
+					<IconPdf/>
+				)}
+
+				{(typeFile === "doc" || typeFile === "docx") && (
+					<IconWord/>
+				)}
+
+				{(typeFile === "xls" || typeFile === "xlsx") && (
+					<IconExcel/>
+				)}
 				<a
 					href={value}
 					target="_blank"
 					rel="noreferrer noopener"
 					className="ml-2 w-[380px] text-indigo-500 text-sm dark:text-indigo-400 hover:underline overflow-hidden whitespace-nowrap"
 				>
-					{value}
+					{value.split("/").pop()}
 				</a>
 				<button
 					onClick={() => onChange && onChange("")}
 					className="bg-rose-500 text-white p-1
-                    rounded-full absolute top-0 right-0 shadow-sm"
+                    rounded-full absolute top-[-10px] right-[-10px] shadow-sm"
 					type="button"
 				>
 					<X className="h-4 w-4" />
@@ -106,6 +135,8 @@ export const FileUpload = ({
 				type="file"
 				onChange={handleFileChange}
 				className="col-span-4 bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+				accept={typeFile}
+				required={required}
 			/>
 			<Button className="col-span-1" variant="primary" onClick={handleUpload}>
 				Up file
